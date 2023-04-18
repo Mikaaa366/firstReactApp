eb9175dd390d2292c122e26fa8b2c6432d5f4a91
Dominika Janisz <mikaaa366@gmail.com>
2023-04-18 23:16:50 +0200

    card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,4 +1,5 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
 
 const Column = props => {
     return (
@@ -6,6 +7,9 @@ const Column = props => {
           <h2 className={styles.title}>
           <span className={styles.icon + ' fa fa-' + props.icon} />
           {props.title}</h2>
+          <ul className={styles.cards}>
+          {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+          </ul>
             
         </article>
     );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,45 @@
 import styles from './List.module.scss'; 
 import Column from '../Column/Column';
+import ColumnForm from '../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
+
 const List = () => {
+  const [columns, setColumns] = useState( [
+  	{
+      id: 1,
+      title: 'Books',
+      icon: 'book',
+      cards: [
+        { id: 1, title: 'This is Going to Hurt' },
+        { id: 2, title: 'Interpreter of Maladies' }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Movies',
+      icon: 'film',
+      cards: [
+        { id: 1, title: 'Harry Potter' },
+        { id: 2, title: 'Star Wars' }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Games',
+      icon: 'gamepad',
+      cards: [
+        { id: 1, title: 'The Witcher' },
+        { id: 2, title: 'Skyrim' }
+      ]
+    }
+  ]);
+
+  const addColumn = newColumn => {
+    setColumns([...columns, {id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+  };
+  
+
     return (
       <div className={styles.list}>
         <header className={styles.header}>
@@ -8,10 +47,10 @@ const List = () => {
         </header>
         <p className={styles.description}>Interesting things I want to check out</p>
         <section className={styles.columns}>
-          <Column title="Books" icon='book' />
-          <Column title="Movies" icon='film' />
-          <Column title="Games" icon ='gamepad' />
+          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} cards={column.cards} />)}
         </section>
+        <ColumnForm action={addColumn} />
+    
       </div>
   );
 };
