8438f8fe48ae8f8df3c07d9d93c933e2d79101cd
Dominika Janisz <mikaaa366@gmail.com>
2023-05-07 08:46:14 +0200

    navlink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import styles from './Navbar.module.scss';
 import Container from '../Container/Container';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -10,15 +12,9 @@ const Navbar = () => {
             <span className='fa fa-tasks'></span>
           </a>
           <ul>
-            <li>
-              <a href='/'>Home</a>
-            </li>
-            <li>
-              <a href='/favorite'>Favorite</a>
-            </li>
-            <li>
-              <a href='/about'>About</a>
-            </li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
           </ul>
         </div>
       </Container>
