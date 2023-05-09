2a16f65bd440ff52787f26241d4c48c1d48e66e8
Dominika Janisz <mikaaa366@gmail.com>
2023-05-09 22:15:57 +0200

    subreducery

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,8 @@ import ColumnForm from '../ColumnForm/ColumnForm';
 import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
 //import { getAllColumns } from '../../redux/store';
-import { getColumnsByList, getListById } from '../../redux/store';
+import { getListById } from '../../redux/listsRedux';
+import { getColumnsByList } from '../../redux/columnsRedux';
 import { useParams } from 'react-router';
 import { Navigate } from 'react-router';
 
---
+++ src/redux/cardsRedux.js
@@ -0,0 +1,26 @@
+import shortid from "shortid";
+import strContains from '../utils/strContains';
+
+export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+
+export const getFavoriteCard = (state) => state.cards.filter((card) => card.isFavorite === true);
+export const toggleCardFavorite = (payload) => ({type: 'TOGGLE_CARD_FAVORITE', payload,});
+
+const createActionName = actionName => `app/cards/${actionName}`;
+const ADD_CARD = createActionName('ADD_CARD');
+
+export const addCard = payload => ({type: ADD_CARD, payload });
+
+const cardsReducer = (statePart = [], action) => {
+    switch(action.type) {
+      case ADD_CARD:
+        return [...statePart, { ...action.payload, id: shortid() }];
+      case 'TOGGLE_CARD_FAVORITE':
+        return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+      default:
+        return statePart;
+    }
+  }
+
+export default cardsReducer;
